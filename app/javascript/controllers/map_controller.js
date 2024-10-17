d597c246e9a3fd4057df4730c5d2d124184afcc1
mathew newton <mattynewts@gmail.com>
2024-10-17 18:17:59 +0800

    styling hotel containers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -24,6 +24,7 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
@@ -32,6 +33,8 @@ export default class extends Controller {
     });
 
   }
+
+
   // #fitMapToMarkers() {
   //   const bounds = new mapboxgl.LngLatBounds()
   //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,21 @@ puts "creating 3 hotels"
 
 Hotel.create(
     name: "The Savoy",
-    latitude: 51.5101,
-    longitude: -0.1205,
+    address: "Strand, London WC2R 0EZ",
     crime_rating: 4.5,
     image: "https://www.livingnorth.com/images/media/articles/life-and-style/travel/The%20Savoy/Savoy%201.jpg?",
     user: freddy)
 
 Hotel.create(
     name: "The Ritz London",
-    latitude: 51.5074,
-    longitude: -0.1419,
+    address: "150 Piccadilly, London W1J 9BR",
     crime_rating: 4.7,
     image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/09/b6/ea/the-ritz-london-exterior.jpg?w=700&h=-1&s=1",
     user: paul
 )
 Hotel.create(
     name: "Claridge's",
-    latitude: 51.5128,
-    longitude: -0.1474,
+    address: "Brook St, London W1K 4HR",
     crime_rating: 4.6,
     image: "https://www.claridges.co.uk/globalassets/claridges.jpg",
     user: john
